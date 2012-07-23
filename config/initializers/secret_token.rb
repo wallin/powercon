b708d4394fd28123c140261589665e311db73734
Sebastian Wallin <sebastian.wallin@gmail.com>
2012-07-23 22:14:42 +0200

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PowerCon::Application.config.secret_token = '5ed160e14cb7148ea065a2767cf792080bd83d72764953edba266358e5b51c327f9459fadf5b596b8e922f8023277e76f606ebd0739db1a7918834fb1f49eb03'
